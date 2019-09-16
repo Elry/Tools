690fd8712496eee2e6fa2f2fb587dee6d8b011d7
RGL <RafaelGarciaLima@outlook.com>
2019-09-16 16:20:36 -0300

    Create charsetHell.sql

---
+++ charsetHell.sql
@@ -0,0 +1,19 @@
+-- checking tables 
+SHOW VARIABLES WHERE Variable_name LIKE 'character\_set\_%' OR Variable_name LIKE 'collation%';
+
+-- simple conn
+mysql -u user -p
+
+-- file && configs
+etc/mysql/my.cnf
+
+[client]
+default-character-set = utf8mb4
+
+[mysql]
+default-character-set = utf8mb4
+
+[mysqld]
+character-set-client-handshake = FALSE
+character-set-server = utf8mb4
+collation-server = utf8mb4_unicode_ci
