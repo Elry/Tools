defa6e998f200110ec33a9735ac8822f1fc902d8
RGL <RafaelGarciaLima@outlook.com>
2019-09-16 17:15:17 -0300

    Update charsetHell.sql

--- charsetHell.sql
+++ charsetHell.sql
@@ -23,3 +23,21 @@ collation-server = utf8mb4_unicode_ci
 -- apply
 service restart mysqld
 service restart mysql
+
+-- DB dumper
+mysqldump --verbose -h host -u username -p --add-drop-database --opt --skip-set-charset --default-character-set=latin1
+  --skip-extended-insert databasename > sqlDump.sql;
+
+-- table dumper
+mysqldump --verbose -h host -u username -p --add-drop-database --opt --skip-set-charset --default-character-set=latin1
+  --skip-extended-insert databasename tablename > sqlDump.sql;
+  
+-- change latin to utf8mb4
+perl -i -pe 's/DEFAULT CHARACTER SET latin1/DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci/' dump_file.sql
+perl -i -pe 's/DEFAULT CHARSET=latin1/DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC/' dump_file.sql
+
+-- restore
+mysql --verbose -h host -u username -p < sqlDump.sql
+
+-- check && optimize
+mysqlcheck --verbose -h host -u root -p --auto-repair --optimize --all-databases
